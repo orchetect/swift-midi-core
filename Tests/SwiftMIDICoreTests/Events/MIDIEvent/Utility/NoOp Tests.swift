5b45d226f8a3c1c889d8a042eab454df8897ad24
Steffan Andrews <stef@orchetect.com>
2026-04-20 15:19:44 -0700

    Tests migration from MIDIKit

---
+++ Tests/SwiftMIDICoreTests/Events/MIDIEvent/Utility/NoOp Tests.swift
@@ -0,0 +1,33 @@
+//
+//  NoOp Tests.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//  © 2021-2025 Steffan Andrews • Licensed under MIT License
+//
+
+@testable import SwiftMIDICore
+import Testing
+
+@Suite struct MIDIEventNoOp_Tests {
+    // swiftformat:options --wrapcollections preserve
+    
+    typealias NoOp = MIDIEvent.NoOp
+    
+    @Test
+    func noOp() {
+        for grp: UInt4 in 0x0 ... 0xF {
+            let event: MIDIEvent = .noOp(group: grp)
+            
+            #expect(
+                event.umpRawWords(protocol: .midi2_0) ==
+                    [[
+                        UMPWord(
+                            0x00 + grp.uInt8Value,
+                            0x00,
+                            0x00,
+                            0x00
+                        )
+                    ]]
+            )
+        }
+    }
+}
---
+++ Tests/SwiftMIDICoreTests/Types/UInt7Pair Tests.swift
@@ -0,0 +1,19 @@
+//
+//  UInt7Pair Tests.swift
+//  MIDIKit • https://github.com/orchetect/MIDIKit
+//  © 2021-2025 Steffan Andrews • Licensed under MIT License
+//
+
+import SwiftMIDICore
+import Testing
+
+@Suite struct UInt7Pair_Tests {
+    @Test
+    func uInt14Value() {
+        let pair = UInt7Pair(msb: 0x7F, lsb: 0x7F)
+        
+        let uInt14 = pair.uInt14Value
+        
+        #expect(uInt14 == UInt14.max)
+    }
+}
