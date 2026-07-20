0151a3ba44176a17641f286bc8dd49f8da60b027
Steffan Andrews <stef@orchetect.com>
2026-07-19 19:06:10 -0700

    Added new `MIDIEventType` enumeration

---
+++ Sources/SwiftMIDICore/Events/MIDIEventType/MIDIEventType.swift
@@ -0,0 +1,26 @@
+//
+//  MIDIEventType.swift
+//  SwiftMIDI Core • https://github.com/orchetect/swift-midi-core
+//  © 2026 Steffan Andrews • Licensed under MIT License
+//
+
+/// MIDI event type.
+public enum MIDIEventType {
+    case channelVoice(MIDIEvent.ChanVoiceType)
+    case systemCommon(MIDIEvent.SysCommonType)
+    case systemExclusive(MIDIEvent.SysExType)
+    case systemRealTime(MIDIEvent.SysRealTimeType)
+    case utility(MIDIEvent.UtilityType)
+}
+
+extension MIDIEventType: Equatable { }
+
+extension MIDIEventType: Hashable { }
+
+extension MIDIEventType: Identifiable {
+    public var id: Self {
+        self
+    }
+}
+
+extension MIDIEventType: Sendable { }
